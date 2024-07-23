5303ba539e5e782e8074c4a58f13bee4b4d45383
shihd1 <46411183+shihd1@users.noreply.github.com>
2024-07-22 22:29:30 -0500

    connect to openai

--- GPTcalls.py
+++ GPTcalls.py
@@ -25,32 +25,9 @@ class GPTAssistant:
             ]
         )
         return response.choices[0].message.content
-        response = self.client.chat.completions.create(
-            model = self.deployment,
-            messages=[
-                {"role": "system", "content": f"""
-                You serve as a tool for filtering, organizing and structuring texts.
-                """},
-                {"role": "user", "content": f"""
-                A large document was divided into parts and the inputs are:
-                Previous: Part of the previous text partition.
-                Actual: Partition the current text.
-                Next: Part of the following text partition.
-                The strategy is to use the current partition with parts of the texts that surround it to create context.
-                You must rewrite the text considering the information provided in order to filter the text in order to generate a consolidated and clean text output.
-                Follow the following instructions:
-                1. Try to maintain the structure of the text provided as much as possible.
-                2. When identifying that this is information about the cover, back cover, summary, dedication, Epigraph, Preface and Presentation of a document, article or book, do not include this information.
-                3. Please do not reference Figures, Tables, Sections and Images in the generated text (e.g.: "As seen in Figure x ...", "As seen in Table x ...", "This text presents ...")
-                4. When checking unfinished information, remove it.
-                5. Do not include information about authors.
-                6. Prioritize maintaining a good context.
-                Given this information:
-                * Previous text: {previous}
-                * Actual text: {actual}
-                * Next text: {next}
-                Create output text taking into account the guidelines above.
-                """}
-            ]
-        )
-        return response.choices[0].message.content
+    
+if __name__ == "__main__":
+    load_dotenv()
+    model = os.environ.get("OPENAI_DEPLOY")
+    helper = GPTAssistant(deployment = model)
+    print(helper.answer_gpt("What troubleshooting steps should I take if my centrifugal pump is experiencing low flow?"))
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import streamlit as st
 import os
+import json
+from GPTcalls import GPTAssistant
+
+# Load json
+with open('prompts.json', 'r') as file:
+    data = json.load(file)
+questions = data['questions']
+prompts = data['prompts']
 
 # Page Header Title
 st.set_page_config(page_title="Fine-Tuning Survey", layout="wide")
-
 st.header("Fine-Tuning Answer User Survey")
 
 # Question
-question_list = ["This is a question", "This is another question", "Create your own"]
-question = st.selectbox("Select a question to answer", options=question_list, index=None)
+select_question = st.selectbox("Select a question to answer", options=questions, index=None)
 
-if question == "Create your own":
+if select_question == "Create your own":
     question = st.text_area("Type your question here")
 
-if question:
+if select_question:
     # Generate list of answers here
-    example_answer_list = ['a', 'b', 'c', 'd', '54j3ryf8e39dj59u547y6fj594tyuj4958tfyu9t8yjfdt9w845tjf9458y7tk2d389yjtf3otyk3d8wt4j7f3958ty7kd2387tjf8357tkd3284957yjf3985ytkd2834y76kf32809-tk389056y7jg2983ykdt389tj7fy32-897tkf89352y7t4395jtyf98wytkdwetyujfw38utfk3-908tuyjgq30-8utkfw038ytgw8903uyftk0-w85utyjg0w84utk3fw09tfjk380wyjg83w09-kft0-3467hq034j7gyw-8307t4kf3w2047k6fj803-5jy6f8-027j65ft-84jgt']
+    complete_prompts = [select_question] + [select_question + " " + prompt for prompt in prompts]
+    answers = []
 
-    columns = st.columns(5)
+    model = os.environ.get("OPENAI_DEPLOY")
+    helper = GPTAssistant(deployment = model)
+    for cp in complete_prompts:
+        answers.append(helper.answer_gpt(cp))
+
+    columns = st.columns(len(questions))
     for i in range(0, len(columns)):
         columns[i].header(f"Answer {i+1:02}")
-        columns[i].write(example_answer_list[i])
+        columns[i].write(answers[i])
 
 # Answer
 answer = st.text_area("Type your response here")
