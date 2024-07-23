7cce928e5713a1bff1a96b47b544308ae7a66b29
Joshua Zhou <joshua.zhou@celanese.com>
2024-07-23 08:46:26 -0500

    fix

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 import json
 from GPTcalls import GPTAssistant
 
+if 'selected_question' not in st.session_state:
+    st.session_state.selected_question = ""
+
 # Load json
 with open('prompts.json', 'r') as file:
     data = json.load(file)
 questions = data['questions']
+questions.append('Create your own')
 prompts = data['prompts']
 
 # Page Header Title
@@ -17,19 +21,21 @@ st.header("Fine-Tuning Answer User Survey")
 select_question = st.selectbox("Select a question to answer", options=questions, index=None)
 
 if select_question == "Create your own":
-    question = st.text_area("Type your question here")
+    select_question = st.text_area("Type your question here")
 
-if select_question:
+if select_question and select_question != st.session_state.selected_question:
+    st.session_state.selected_question = select_question
     # Generate list of answers here
     complete_prompts = [select_question] + [select_question + " " + prompt for prompt in prompts]
     answers = []
 
     model = os.environ.get("OPENAI_DEPLOY")
     helper = GPTAssistant(deployment = model)
-    for cp in complete_prompts:
-        answers.append(helper.answer_gpt(cp))
+    with st.spinner("Loading Example Answers"):
+        for cp in complete_prompts:
+                answers.append(helper.answer_gpt(cp))
 
-    columns = st.columns(len(questions))
+    columns = st.columns(len(answers))
     for i in range(0, len(columns)):
         columns[i].header(f"Answer {i+1:02}")
         columns[i].write(answers[i])
